fb53dde6f8c8bc2337c9bc638ed239034e95f0be
janeblancaocampo <133573507+janeblancaocampo@users.noreply.github.com>
2023-05-15 12:34:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,57 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 from datetime import datetime, timedelta
 
-# Load the model
-model = load_model('dengue_lstm_model.h5')
+# Load the trained LSTM model
+model = load_model('/content/drive/MyDrive/3RD YEAR 2ND SEM/Emerging TECH. 2/Datasets/dengue_lstm_model.h5')
 
-# Load the dataset
-data = pd.read_csv('denguecases.csv', index_col='Date', parse_dates=True)
-
-# Compute the sum of dengue cases per region
-sum_by_region = data.groupby('Region')['Dengue_Cases'].sum()
-
-# Compute the total number of dengue cases per day
-total_cases = data.drop('Region', axis=1).sum(axis=1)
-
-# Preprocess the data
+# Load the dataset and preprocess it
+data = pd.read_csv('/content/drive/MyDrive/3RD YEAR 2ND SEM/Emerging TECH. 2/Datasets/denguecases.csv', index_col = 'Date', parse_dates = True)
+whole_data = data.drop('Region', axis = 1)
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(total_cases.values.reshape(-1, 1))
+scaled_data = scaler.fit_transform(whole_data.values)
 
-# Define a function to predict the next 12 months of dengue cases
-def predict_next_12_months(model, data, scaler):
-    last_date = data.index[-1]
-    last_value = data[-1]
-    next_dates = pd.date_range(start=last_date, periods=13, freq='MS')[1:]
-    predictions = []
-    for i in range(12):
-        x = np.array(last_value).reshape(1, 1, 1)
-        x_scaled = scaler.transform(x)
-        yhat_scaled = model.predict(x_scaled)
-        yhat = scaler.inverse_transform(yhat_scaled)[0][0]
-        predictions.append(yhat)
-        last_value = yhat_scaled
-    return next_dates, predictions
+# Define a function to make predictions
+def make_predictions(start_date, n_months):
+    # Convert the start_date to datetime format
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    
+    # Create a list of datetime objects for the next n_months
+    date_list = [start_date + timedelta(days=30*i) for i in range(n_months)]
+    
+    # Create a DataFrame to store the predictions
+    predictions = pd.DataFrame({'Date': date_list})
+    
+    # Predict the number of dengue cases for each month
+    for i in range(n_months):
+        # Get the scaled input data for the last 12 months
+        x = scaled_data[-12:]
+        # Reshape the input data into (samples, time steps, features)
+        x = np.reshape(x, (1, x.shape[0], x.shape[1]))
+        # Make the prediction for the next month
+        y_pred = model.predict(x)
+        # Inverse transform the scaled prediction
+        y_pred = scaler.inverse_transform(y_pred)[0][0]
+        # Add the prediction to the DataFrame
+        predictions.loc[i, 'Dengue_Cases'] = y_pred
+        # Add the next month to the input data for the next prediction
+        next_month = scaled_data[-1, 1:]
+        next_month = np.append(next_month, y_pred)
+        scaled_data = np.vstack((scaled_data, next_month))
+    
+    # Set the Date column as the index
+    predictions.set_index('Date', inplace=True)
+    
+    return predictions
 
-# Predict the next 12 months of dengue cases
-next_dates, predictions = predict_next_12_months(model, total_cases, scaler)
-
-# Add the predictions to the dataset
-next_data = pd.Series(predictions, index=next_dates)
-data = pd.concat([data, next_data], axis=0)
-
-# Plot the data
-fig = px.line(data, x=data.index, y='Dengue_Cases', title='Dengue Cases in the Philippines')
-fig.update_xaxes(title='Date')
-fig.update_yaxes(title='Dengue Cases')
-st.plotly_chart(fig)
-
-# Show the predicted values for the next 12 months
-st.write('Predicted Dengue Cases for the next 12 months:')
-for i in range(len(next_dates)):
-    st.write(f'{next_dates[i].strftime("%b %Y")}: {int(predictions[i]):,}')
+# Define the Streamlit app
+st.title('Dengue Cases Prediction')
+st.write('This app predicts the number of dengue cases in the Philippines for the next 12 months.')
+start_date = st.date_input('Enter the start date for the prediction:', value=datetime(2023,1,1), min_value=datetime(2010, 1, 1), max_value=datetime(2023, 5, 15))
+n_months = 12
+if st.button('Make Prediction'):
+    predictions = make_predictions(start_date, n_months)
+    st.write(predictions)
